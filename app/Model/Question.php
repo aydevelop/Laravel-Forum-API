920f461178abf5124df6fb6dc53482ffcbd73817
ay.develop <ay.develop@gmail.com>
2019-10-21 17:49:01 -0700

    72638476

---
+++ app/Model/Question.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model;
+
+use App\User;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    //protected $fillable = ['title','slug','body','category_id','user_id'];
+
+    protected $guarded = [];
+
+    public function getPathAttribute()
+    {
+        return url('api/question/'.$this->slug);
+    }
+}
